c8ae703828c1dbc4310b7410a383e4a4698d97b1
comaldave <skinner.david@gmail.com>
2014-07-08 11:05:17 -0500

    Tutorial.

---
+++ www.go
@@ -0,0 +1,16 @@
+// www.go
+
+package www
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world! from OTB.")
+}
